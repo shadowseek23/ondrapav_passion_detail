276fe81ba6fc26330cac5a5303f307fd7083b682
[Daniel Sýkora] <[shadowsyd_services@protonmail.com]>
2025-04-10 01:27:30 +0200

    Kompletne pripraveny template stazeny z gymtime. Pridany vzorove prvky od Pobo. Najs.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 // Import styles
+
+import './styles/gymtime/main.less.css';
+import './styles/gymtime/screen.css';
+import './styles/gymtime/inbetween.css';
+import './styles/gymtime/style.css';
+import './styles/gymtime/bespoke.css';
+// import './styles/gymtime/after.css';
+
+import './styles/pobo/generic.css';
+
 import './styles/main.css';
-import './styles/vendor.css';
